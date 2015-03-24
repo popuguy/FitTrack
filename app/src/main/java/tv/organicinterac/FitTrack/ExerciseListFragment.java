9c73164712812469c68fccfc00d1e70cbb919cf3
Paul Cantrell <paulcantr@gmail.com>
2015-03-23 22:22:19 -0700

    Scrapped old MainActivity

--- app/src/main/java/tv/organicinterac/FitTrack/ExerciseListFragment.java
+++ app/src/main/java/tv/organicinterac/FitTrack/ExerciseListFragment.java
@@ -29,153 +29,16 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Paul on 3/17/2015.
  */
-public class ExerciseListFragment extends Fragment implements View.OnClickListener {
-
-    Button bAdd;
-    ImageButton bStart;
-    TextView tvTimer;
-    ListView lvWorkoutItems;
-    RelativeLayout rlNewEvent;
-    EditText etNewExerciseName, etNewExerciseSetsName, etNewExerciseRepsName;
-
-    WorkoutAdapter adapter;
-    WorkoutItemAdapter wia;
-    Thread thread;
-    Date startRun;
-    String origText;
-    boolean running = false;
-
-    List<String[]> rawItems;
+public class ExerciseListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View inflated = inflater.inflate(R.layout.activity_main, container, false);
 
-        bStart = (ImageButton) inflated.findViewById(R.id.start_button);
-        bAdd = (Button) inflated.findViewById(R.id.button);
-        tvTimer = (TextView) inflated.findViewById(R.id.timer_textview);
-        origText = tvTimer.getText().toString();
-        lvWorkoutItems = (ListView) inflated.findViewById(R.id.workout_items_listview);
-        rlNewEvent = (RelativeLayout) inflated.findViewById(R.id.new_event_relativelayout);
-        etNewExerciseName = (EditText) inflated.findViewById(R.id.new_exercise_name_edittext);
-        etNewExerciseSetsName = (EditText) inflated.findViewById(R.id.new_exercise_sets_edittext);
-        etNewExerciseRepsName = (EditText) inflated.findViewById(R.id.new_exercise_reps_edittext);
-        ArrayList<String[]> items = new ArrayList<>();
-        adapter = new WorkoutAdapter(inflated.getContext());
-        rawItems = adapter.getExercises();
-        for (String[] rawItem: rawItems) {
-            items.add(new String[]{rawItem[1], rawItem[2]});
-        }
-        wia = new WorkoutItemAdapter(inflated.getContext(), R.layout.list_item, items);
-
-        lvWorkoutItems.setAdapter(wia);
-
-
-        bAdd.setOnClickListener(this);
-        bStart.setOnClickListener(this);
-
-        Timer textUpdateTimer = new Timer();
-        textUpdateTimer.scheduleAtFixedRate(new TimerTask(){
-
-            @Override
-            public void run() {
-                if (running) {
-                    final int diffHour = (int)TimeUnit.MILLISECONDS.toHours(
-                            (new Date()).getTime() - startRun.getTime());
-                    final int diffMin = (int)TimeUnit.MILLISECONDS.toMinutes(
-                            (new Date()).getTime() - startRun.getTime()) -
-                            (diffHour * 60);
-                    final int diffSec = (int)TimeUnit.MILLISECONDS.toSeconds(
-                            (new Date()).getTime() - startRun.getTime()) -
-                            (diffMin * 60) - (diffHour * 60 * 60);
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tvTimer.setText(String.format("Current time: %02d:%02d:%02d",
-                                    diffHour, diffMin, diffSec));
-                        }
-                    });
-                }
-            }
-        }, 1000, 1000);
         return inflated;
     }
 
-    public void toggleTimer() {
-        if (running) {
-            running = false;
-        } else {
-            startRun = new Date();
-            running = true;
-        }
-    }
-    private void addExercise() {
-        if (rlNewEvent.getVisibility() != View.VISIBLE) {
-            rlNewEvent.setVisibility(View.VISIBLE);
-            ObjectAnimator animator = ObjectAnimator.ofFloat(rlNewEvent, "alpha", 0f, 1f);
-            animator.setDuration(550);
-            animator.start();
-        } else {
-            String[] newExercise = getNewExercise();
-            if (newExercise[0].length() > 0 && newExercise[1].length() > 4){
-                InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(
-                        Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(etNewExerciseRepsName.getWindowToken(), 0);
-                wia.add(newExercise);
-                adapter.addExercise(newExercise[0], newExercise[1]);
-                clearExerciseEntry();
-                ObjectAnimator animator = ObjectAnimator.ofFloat(
-                        rlNewEvent, "alpha", 1f, 0f);
-                animator.setDuration(550);
-                animator.addListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
 
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        rlNewEvent.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation) {
-
-                    }
-                });
-                animator.setStartDelay(200);
-                animator.start();
-            }
-        }
-    }
-    private String[] getNewExercise() {
-        return new String[]{etNewExerciseName.getText().toString(),
-                etNewExerciseSetsName.getText().toString() + " x " +
-                        etNewExerciseRepsName.getText().toString()};
-    }
-
-    private void clearExerciseEntry() {
-        etNewExerciseName.setText("");
-        etNewExerciseRepsName.setText("");
-        etNewExerciseSetsName.setText("");
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.start_button:
-                toggleTimer();
-                break;
-            case R.id.button:
-                addExercise();
-                break;
-        }
-    }
     private void toast(String out) {
         //cuz I'm real lazy like that
         Toast.makeText(getActivity().getApplicationContext(), out, Toast.LENGTH_SHORT).show();
--- app/src/main/java/tv/organicinterac/FitTrack/MainActivity.java
+++ app/src/main/java/tv/organicinterac/FitTrack/MainActivity.java
@@ -27,149 +27,24 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends ActionBarActivity {
 
-    ImageButton bStart;
-    Button bAdd;
-    TextView tvTimer;
-    ListView lvWorkoutItems;
-    RelativeLayout rlNewEvent;
-    EditText etNewExerciseName, etNewExerciseSetsName, etNewExerciseRepsName;
-
-    WorkoutAdapter adapter;
-    WorkoutItemAdapter wia;
-    Thread thread;
-    Date startRun;
-    String origText;
-    boolean running = false;
-
-    List<String[]> rawItems;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-//        getActionBar().hide();
-
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-
         setContentView(R.layout.activity_main);
-        bStart = (ImageButton) findViewById(R.id.start_button);
-        bAdd = (Button) findViewById(R.id.button);
-        tvTimer = (TextView) findViewById(R.id.timer_textview);
-        origText = tvTimer.getText().toString();
-        lvWorkoutItems = (ListView) findViewById(R.id.workout_items_listview);
-        rlNewEvent = (RelativeLayout) findViewById(R.id.new_event_relativelayout);
-        etNewExerciseName = (EditText) findViewById(R.id.new_exercise_name_edittext);
-        etNewExerciseSetsName = (EditText) findViewById(R.id.new_exercise_sets_edittext);
-        etNewExerciseRepsName = (EditText) findViewById(R.id.new_exercise_reps_edittext);
-        ArrayList<String[]> items = new ArrayList<>();
-        adapter = new WorkoutAdapter(getApplicationContext());
-        rawItems = adapter.getExercises();
-        for (String[] rawItem: rawItems) {
-            items.add(new String[]{rawItem[1], rawItem[2]});
-        }
-        wia = new WorkoutItemAdapter(this, R.layout.list_item, items);
 
-        lvWorkoutItems.setAdapter(wia);
-        bAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (rlNewEvent.getVisibility() != View.VISIBLE) {
-                    rlNewEvent.setVisibility(View.VISIBLE);
-                    ObjectAnimator animator = ObjectAnimator.ofFloat(rlNewEvent, "alpha", 0f, 1f);
-                    animator.setDuration(550);
-                    animator.start();
-                } else {
-                    String[] newExercise = getNewExercise();
-                    if (newExercise[0].length() > 0 && newExercise[1].length() > 4){
-                        InputMethodManager imm = (InputMethodManager)getSystemService(
-                                Context.INPUT_METHOD_SERVICE);
-                        imm.hideSoftInputFromWindow(etNewExerciseRepsName.getWindowToken(), 0);
-                        wia.add(newExercise);
-                        adapter.addExercise(newExercise[0], newExercise[1]);
-                        clearExerciseEntry();
-                        ObjectAnimator animator = ObjectAnimator.ofFloat(rlNewEvent, "alpha", 1f, 0f);
-                        animator.setDuration(550);
-                        animator.addListener(new Animator.AnimatorListener() {
-                            @Override
-                            public void onAnimationStart(Animator animation) {
 
-                            }
-
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                rlNewEvent.setVisibility(View.GONE);
-                            }
-
-                            @Override
-                            public void onAnimationCancel(Animator animation) {
-
-                            }
-
-                            @Override
-                            public void onAnimationRepeat(Animator animation) {
-
-                            }
-                        });
-                        animator.setStartDelay(200);
-                        animator.start();
-                    }
-                }
-            }
-        });
-        Timer textUpdateTimer = new Timer();
-        textUpdateTimer.scheduleAtFixedRate(new TimerTask(){
-
-            @Override
-            public void run() {
-                if (running) {
-                    final int diffHour = (int)TimeUnit.MILLISECONDS.toHours(
-                            (new Date()).getTime() - startRun.getTime());
-                    final int diffMin = (int)TimeUnit.MILLISECONDS.toMinutes(
-                            (new Date()).getTime() - startRun.getTime()) -
-                            (diffHour * 60);
-                    final int diffSec = (int)TimeUnit.MILLISECONDS.toSeconds(
-                            (new Date()).getTime() - startRun.getTime()) -
-                            (diffMin * 60) - (diffHour * 60 * 60);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tvTimer.setText(String.format("Current time: %02d:%02d:%02d",
-                                    diffHour, diffMin, diffSec));
-                        }
-                    });
-                }
-            }
-        }, 1000, 1000);
     }
-
-
-    public void startTimer(View v) {
-        if (running) {
-            running = false;
-        } else {
-            startRun = new Date();
-            running = true;
-        }
-    }
-
     @Override
     protected void onPause() {
         super.onPause();
     }
 
-    private String[] getNewExercise() {
-        return new String[]{etNewExerciseName.getText().toString(),
-                etNewExerciseSetsName.getText().toString() + " x " +
-                        etNewExerciseRepsName.getText().toString()};
-    }
 
-    private void clearExerciseEntry() {
-        etNewExerciseName.setText("");
-        etNewExerciseRepsName.setText("");
-        etNewExerciseSetsName.setText("");
-    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -186,9 +61,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
